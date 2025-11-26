122aff7239e1a4a00fd8c5524593f8edbf7eeb0d
lifbasya <alifattallahbasya9>
2025-11-26 11:17:32 +0700

    middleware

--- src/middlewares/errorMiddleware.js
+++ src/middlewares/errorMiddleware.js
@@ -1,5 +1,5 @@
 export const errorMiddleware = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
+    const statusCode = err.status || 500;
     const bodyStatus = statusCode <= 500 ? "fail" : "error";
     const message = err.message || "Internal Server Error";
     res.status(statusCode).json({
